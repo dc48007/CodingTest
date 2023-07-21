c48ae7f0644e08759323508b013ddc7bbb5f67ce
deepakc <deepak.chaudhary@lendingtree.com>
2023-07-21 14:20:24 +0530

    Initial class created

---
+++ src/main/java/Practise/QueueDS/PracticeToWorkInSharing.java
@@ -0,0 +1,16 @@
+package Practise.QueueDS;
+public class PracticeToWorkInSharing {
+
+
+
+    public void initialProgramCreated(){
+        System.out.println("this is existing program");
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
