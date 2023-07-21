cdb8095e1ceedf5b88ccc00dd555cec488a82a7b
deepakc <deepak.chaudhary@lendingtree.com>
2023-07-21 14:26:29 +0530

    emp2 changes

--- src/main/java/Practise/QueueDS/PracticeToWorkInSharing.java
+++ src/main/java/Practise/QueueDS/PracticeToWorkInSharing.java
@@ -5,6 +5,7 @@ public class PracticeToWorkInSharing {
 
     public void initialProgramCreated(){
         System.out.println("Emp03 has changes the lines");
+        System.out.println("Emp1 added the changes");
         System.out.println("this is existing program");
     }
 
