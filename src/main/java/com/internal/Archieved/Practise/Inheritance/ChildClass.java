4bc1e52170fa7f05e0b2e0f0b5a1e5f4746328ca
deepakc <deepak.chaudhary@Celsiortech.com>
2024-10-15 17:42:45 +0530

    practise program - Changes - logger, csv

--- src/main/java/com/internal/Archieved/Practise/Inheritance/ChildClass.java
+++ src/main/java/com/internal/Archieved/Practise/Inheritance/ChildClass.java
@@ -8,12 +8,17 @@ public class ChildClass extends SuperClass {
         System.out.println("child value is: " + name);
     }
 
+    public void childClassMethod(){
+        System.out.println("child class method");
+    }
+
     public static void main(String[] args) {
         SuperClass c1 = new SuperClass(1, 2);
         System.out.println(c1.get());
         c1.name = "Ram";
         c1.display();
         SuperClass superClass = new ChildClass();
+        ((ChildClass) superClass).childClassMethod();
         superClass.name = "Shaam";
         superClass.display();
     }
